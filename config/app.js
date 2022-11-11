67d21270c03a30a1c621460e480a1caad7a8e217
frzn1717 <99238451+frzn1717@users.noreply.github.com>
2022-11-11 12:54:25 -0500

    product list

---
+++ config/app.js
@@ -0,0 +1,46 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session'); //ACA 11092022
+var flash = require('connect-flash'); //ACA 11092022
+var passport = require('passport'); //ACA 11092022
+
+var indexRouter = require('../routes/index');
+var usersRouter = require('../routes/users');
+var addEditRouter = require('../routes/add_edit.router'); //ACA 11092022
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../node_modules')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/product', addEditRouter); //ACA 11092022
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
