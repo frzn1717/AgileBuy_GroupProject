666d812fc6fc407d77b9ed119d8c7bdb8537e7f3
Natoyam <natoya.maynard8@gmail.com>
2022-11-11 19:55:52 -0500

    edit

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,40 +1,38 @@
 exports.addEdit = function(req, res, next) {
     res.render(
-      'product/add_edit', 
-      { 
-        title: 'Add Edit page',
-        fullName: 'Arvin Almario',
-        contactNumber: '09193033871',
-        itemName: 'Iphone14',
-        brand:'Phones',
-        category: 'Electronics',
-        quantity: '5',
-        price: '1000',
-        addDate: 'Nov. 09 2022', 
-        location: 'Ontario',
-        description: 'for sale, 9 months used, need to go, broken',
-      }
+        'product/add_edit', {
+            title: 'Add Edit page',
+            fullName: 'Arvin Almario',
+            contactNumber: '09193033871',
+            itemName: 'Iphone14',
+            brand: 'Phones',
+            category: 'Electronics',
+            quantity: '5',
+            price: '1000',
+            addDate: 'Nov. 09 2022',
+            location: 'Ontario',
+            description: 'for sale, 9 months used, need to go, broken',
+        }
     );
-  };
+};
 
-  exports.displayList = function(req, res, next) {
+exports.displayList = function(req, res, next) {
     res.render(
-      'product/edit', 
-      { 
-        title: 'List of Products',
-        fullName: 'Arvin Almario',
-        contactNumber: '09193033871',
-        itemName: 'Iphone14',
-        brand:'Phones',
-        category: 'Electronics',
-        quantity: '5',
-        price: '1000',
-        addDate: 'Nov. 09 2022', 
-        location: 'Ontario',
-        description: 'for sale, 9 months used, need to go, broken',
-      }
+        'product/edit', {
+            title: 'List of Products',
+            fullName: 'Arvin Almario',
+            contactNumber: '09193033871',
+            itemName: 'Iphone14',
+            brand: 'Phones',
+            category: 'Electronics',
+            quantity: '5',
+            price: '1000',
+            addDate: 'Nov. 09 2022',
+            location: 'Ontario',
+            description: 'for sale, 9 months used, need to go, broken',
+        }
     );
-  };
+};
 
 
 
